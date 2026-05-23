f55833c4f9bf67f30a2add454ace349a9946a097
Nicolas-Monteiro <nikmonteiro27@gmail.com>
2026-05-22 23:12:32 -0300

    adiciona pagina de classificacao com graficos analiticos

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,6 +6,7 @@ import dash
 import dash_bootstrap_components as dbc
 import eda
 import xgboost_page
+import classificacao_page 
 
 df = pd.read_parquet('Imdb_Movie_Dataset.parquet')
 
@@ -27,7 +28,7 @@ SIDEBAR_STYLE = {
 }
 
 CONTENT_STYLE = {
-    "marginLeft": "9rem",
+    "marginLeft": "18rem", 
     "marginRight": "2rem",
 }
 
@@ -41,7 +42,8 @@ sidebar = html.Div(
                 dbc.NavLink("Exploratória (EDA)", href="/eda", active="exact"),
                 dbc.NavLink("Clusterização", href="/cluster", active="exact", disabled=True),
                 dbc.NavLink("Regressão", href="/regression", active="exact", disabled=True),
-                dbc.NavLink("XGBoost", href="/xgboost_page", active="exact"),
+                dbc.NavLink("Métricas (XGBoost)", href="/xgboost_page", active="exact"),
+                dbc.NavLink("Classificação (LightGBM)", href="/classificacao", active="exact"),
             ],
             vertical=True,
             pills=True,
@@ -73,7 +75,7 @@ content_home = html.Div([
     html.Div([
         html.H4("Significado das Colunas Principais", className="mt-4"),
         html.Ul([
-html.Li([html.B("id:"), " Identificador único de cada filme no banco de dados."]),
+            html.Li([html.B("id:"), " Identificador único de cada filme no banco de dados."]),
             html.Li([html.B("title:"), " Título oficial do filme em seu lançamento comercial."]),
             html.Li([html.B("vote_average:"), " Nota média dada pelos usuários (0-10)."]),
             html.Li([html.B("vote_count:"), " Número total de votos e avaliações recebidas pelo filme."]),
@@ -111,6 +113,8 @@ def render_page_content(pathname):
         return eda.create_eda_layout(df)
     elif pathname == "/xgboost_page":
         return xgboost_page.create_xgboost_layout()
+    elif pathname == "/classificacao":                     
+        return classificacao_page.create_lgbm_layout()
     
     return html.Div(
         [
@@ -123,6 +127,7 @@ def render_page_content(pathname):
 
 eda.register_eda_callbacks(app, df)
 xgboost_page.register_xgboost_callbacks(app)
+classificacao_page.register_lgbm_callbacks(app)  
 
 if __name__ == '__main__':
     app.run(debug=True)
