020a0f8860a5c331b008764ea88b8b9c4a1a8f34
Nicacio <nicaciodev313@gmail.com>
2026-05-26 19:44:53 -0300

    correção de espaçamento na eda

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import plotly.express as px 
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, dash_table, Input, Output
 import dash
