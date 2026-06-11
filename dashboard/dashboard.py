7eb69ebb7f242fe42425d1bbc7d613ef93bad2f2
Nicacio <nicaciodev313@gmail.com>
2026-06-11 11:22:54 -0300

    ajuste nas bibliotecas do requirements.txt

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -27,8 +27,7 @@ total_colunas = df.shape[1]
 tamanho_memoria = f"{df.memory_usage(deep=True).sum() / 1024**2:.2f} MB"
 
 
-# --- CONFIGURAÇÃO DO DASH ---
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], suppress_callback_exceptions=True)
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], suppress_callback_exceptions=True)
 
 server = app.server
